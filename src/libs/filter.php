b89493faec60bd6e2e08665a84b5e7210e68fe0b
yapeaud <yapoabed@gmail.com>
2023-02-14 14:15:22 +0000

    w7d5mp

---
+++ src/libs/filter.php
@@ -0,0 +1,22 @@
+ <?php
+
+
+function filter(array $data, array $fields, array $messages = []): array
+{
+    $sanitization = [];
+    $validation = [];
+
+   
+    foreach ($fields as $field => $rules) {
+        if (strpos($rules, '|')) {
+            [$sanitization[$field], $validation[$field]] = explode('|', $rules, 2);
+        } else {
+            $sanitization[$field] = $rules;
+        }
+    }
+   
+    $inputs = sanitize($data, $sanitization);
+    $errors = validate($inputs, $validation, $messages);
+
+    return [$inputs, $errors];
+}
